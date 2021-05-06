1065ac9b2c7f7a449d441e61513b623c00f3015d
Moe Farid <moe.faridd@gmail.com>
2021-05-06 01:02:57 -0400

    2

--- HelloWorld3/Program.cs
+++ HelloWorld3/Program.cs
@@ -6,7 +6,11 @@ namespace HelloWorld3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("{0}, {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0}, {1}", float.MinValue, float.MaxValue);
+            
+
+
         }
     }
 }
